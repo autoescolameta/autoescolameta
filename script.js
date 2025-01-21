446aad9fcfea96c814d10ed3f40218cd95d7c7ae
autoescolameta <autoescolameta2@gmail.com>
2025-01-21 14:02:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const home = document.getElementById("home");
+const sobre = document.getElementById("sobres");
+const plano = document.getElementById("plano");
+
+home.classList.add("home")
+
+sobre.addEventListener("click", () =>{
+    sobre.classList.add("sobre1")
+    home.classList.remove("home")
+    plano.classList.remove("plano")
+})
+sobre.addEventListener("click", () =>{
+    sobre.classList.add("sobre1")
+    home.classList.remove("home")
+    plano.classList.remove("plano")
+})
+plano.addEventListener("click", () =>{
+    sobre.classList.remove("sobre1")
+    home.classList.remove("home")
+    plano.classList.add("plano")
+})
+
+home.addEventListener("click", () =>{
+    sobre.classList.remove("sobres")
+    home.classList.add("home")
+    plano.classList.remove("plano")
+})
